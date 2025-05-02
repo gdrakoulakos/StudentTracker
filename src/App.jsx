347cbc848529b005418720f4f40bedc12a144729
George <georgediopan@gmail.com>
2025-05-03 00:20:23 +0300

    Initial create "Student List" and import it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { StudentList } from "./components/StudentList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <StudentList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
